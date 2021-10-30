dd27e3e025b0e34d8f041c52b80170569e960645
Marta <martagaworek05@gmail.com>
2021-10-30 23:23:36 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
 function App() {
+const [array, setArray] = React.useState([])
+const [object, setObject] = React.useState({})
+
+
+React.useEffect(() => {
+  fetch('https://simpsons-quotes-api.herokuapp.com/quotes?count=10')
+  .then((response)=>{
+    return response.json()
+  })
+  .then((arrayOfObjects) => {
+    let randomIndex = Math.floor(Math.random() * (arrayOfObjects.length-1))
+    setArray(arrayOfObjects)
+    setObject(arrayOfObjects[randomIndex])
+  });
+}, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>character : {object.character}</p>
+      <p>quote : {object.quote}</p>
+      <p><img src={object.image}  alt="" /></p>
+    <button onClick={()=> 
+    setObject(array[Math.floor(Math.random() * (array.length-1))])}>
+    Random Quote</button>
     </div>
   );
 }
